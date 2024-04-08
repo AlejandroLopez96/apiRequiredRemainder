0d722257f376015563c285835e012c54e46ceee3
Alejandro López Ortiz <alexlopezortiz9@gmail.com>
2024-04-08 12:43:08 +0200

    Update Utils.ts

--- front-required-remainder/src/app/utils/Utils.ts
+++ front-required-remainder/src/app/utils/Utils.ts
@@ -1,9 +1,4 @@
 export class Utils {
-
-    static validateFieldsNumber() {
-
-    }
-    
     static valueIsNotNumber(value: any) {
         if(typeof value !== "number") return true; else return false;
     }
