c75d630f416e17e20008d7fc085c84828e248716
Alejandro López Ortiz <alexlopezortiz9@gmail.com>
2024-04-08 18:00:58 +0200

    Delete unused code

--- required-remainder/src/main/java/com/api/requiredremainder/controller/UserController.java
+++ required-remainder/src/main/java/com/api/requiredremainder/controller/UserController.java
@@ -1,18 +1,11 @@
 package com.api.requiredremainder.controller;
 
-import java.security.Principal;
-import java.util.Base64;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.requiredremainder.model.User;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @CrossOrigin
@@ -20,7 +13,5 @@ public interface UserController {
 
     @PostMapping("/login")
     public boolean login(@RequestBody User user);
-	
-    @GetMapping("/user")
-    public Principal user(HttpServletRequest request);
+
 }
--- required-remainder/src/main/java/com/api/requiredremainder/controller/impl/UserControllerImpl.java
+++ required-remainder/src/main/java/com/api/requiredremainder/controller/impl/UserControllerImpl.java
@@ -1,16 +1,10 @@
 package com.api.requiredremainder.controller.impl;
 
-import java.security.Principal;
-import java.util.Base64;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.requiredremainder.controller.UserController;
 import com.api.requiredremainder.model.User;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class UserControllerImpl implements UserController {
@@ -19,13 +13,5 @@ public class UserControllerImpl implements UserController {
   public boolean login(@RequestBody User user) {
       return
         user.getUsername().equals("admin") && user.getPassword().equals("password");
-  }
-
-  @Override
-  public Principal user(HttpServletRequest request) {
-      String authToken = request.getHeader("Authorization")
-        .substring("Basic".length()).trim();
-      return () ->  new String(Base64.getDecoder()
-        .decode(authToken)).split(":")[0];
   }
 }
